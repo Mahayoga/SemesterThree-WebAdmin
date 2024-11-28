6b9d689b93737ec78b5f83bd82f5c7c74307a3c6
Mahayoga <myoga.bahtiar@gmail.com>
2024-11-28 20:54:57 +0700

    Add, Change: README.md buat tutor

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php 
-  session_start();
-  if($_SESSION['isLogin']) {
+  // session_start();
+  // if($_SESSION['isLogin']) {
     include 'pages/layout/app.php';
-  } else {
-    include 'pages/user/index.php';
-  }
+  // } else {
+  //   include 'pages/user/index.php';
+  // }
 ?>
