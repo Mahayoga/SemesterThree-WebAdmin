f978d52918c3d4620d1a22b11773b7c7ce80171b
flndyn <filaindayani9@gmail.com>
2024-12-02 18:56:43 +0700

    data tabel user

--- pages/admin/datauser.php
+++ pages/admin/datauser.php
@@ -1,10 +1,19 @@
 <div class="row">
-  <div class="col-12">
+<?php
+// Mengimpor file koneksi database
+include 'config/connection.php';
+
+// Query untuk mendapatkan data dari tabel users
+$sql = "SELECT * FROM users";
+$result = $koneksi->query($sql);
+?>
+<div class="col-12">
     <div class="card mb-4">
       <div class="card-header pb-0">
-      <h6><Strong>Data User</Strong></h6>
-        <div><button type="button" class="btn btn-success btn-sm"> Tambah</button>
-</div>
+        <h6><strong>Data User</strong></h6>
+        <div>
+          <button type="button" class="btn btn-success btn-sm">Tambah</button>
+        </div>
       </div>
       <div class="card-body px-0 pt-0 pb-2">
         <div class="table-responsive p-0">
@@ -16,224 +25,56 @@
                 <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">No Telepon</th>
                 <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Email</th>
                 <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Password</th>
+                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Role</th>
                 <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Aksi</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>
-                  <div class="d-flex px-2 py-1">
-                    <div>
-
-                    </div>
-                    <div class="d-flex flex-column justify-content-center">
-                      <h6 class="mb-0 text-sm">1</h6>
-
-                    </div>
-                  </div>
-                </td>
-                <td>
-                  <p class="text-xs font-weight-bold mb-0">Mahayoga</p>
-                </td>
-                <td class="align-middle text-center text-sm">
-                  <span class="badge badge-sm bg-gradient-success">0812345671</span>
-                </td>
-                <td class="align-middle text-center">
-                  <span class="text-secondary text-xs font-weight-bold">myoga.bahtiar@gmail.com</span>
-                </td>
-                <td class="align-middle text-center">
-                  <span class="text-secondary text-xs font-weight-bold">admin1234</span>
-                </td>
-                <td class="align-middle">
-                  <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
-                
-                  <button type="button" class="btn btn-primary btn-sm">Edit</button>
-                  <button type="button" class="btn btn-danger btn-sm">Hapus</button>
-                  <button type="button" class="btn btn-dark btn-sm">Detail</button>
-                
-                  </a>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div class="d-flex px-2 py-1">
-                    <div>
-                  
-                    </div>
-                    <div class="d-flex flex-column justify-content-center">
-                      <h6 class="mb-0 text-sm">2</h6>
-                 
-                    </div>
-                  </div>
-                </td>
-                <td>
-                  <p class="text-xs font-weight-bold mb-0">Irsyad</p>
-       
-                </td>
-                <td class="align-middle text-center text-sm">
-                  <span class="badge badge-sm bg-gradient-success">0812345672</span>
-                </td>
-                <td class="align-middle text-center">
-                  <span class="text-secondary text-xs font-weight-bold">syadd@gmail.com</span>
-                </td>
-                <td class="align-middle text-center">
-                  <span class="text-secondary text-xs font-weight-bold">admin1234</span>
-                </td>
-                <td class="align-middle">
-                  <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
-                
-                  <button type="button" class="btn btn-primary btn-sm">Edit</button>
-                  <button type="button" class="btn btn-danger btn-sm">Hapus</button>
-                  <button type="button" class="btn btn-dark btn-sm">Detail</button>
-                
-                  </a>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div class="d-flex px-2 py-1">
-                    <div>
-
-                    </div>
-                    <div class="d-flex flex-column justify-content-center">
-                      <h6 class="mb-0 text-sm">3</h6>
-
-                    </div>
-                  </div>
-                </td>
-                <td>
-                  <p class="text-xs font-weight-bold mb-0">Nisa</p>
-       
-                </td>
-                <td class="align-middle text-center text-sm">
-                  <span class="badge badge-sm bg-gradient-success">0812345673</span>
-                </td>
-                <td class="align-middle text-center">
-                  <span class="text-secondary text-xs font-weight-bold">nisa@gmail.com</span>
-                </td>
-                <td class="align-middle text-center">
-                  <span class="text-secondary text-xs font-weight-bold">admin1234</span>
-                </td>
-                <td class="align-middle">
-                  <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
-                
-                  <button type="button" class="btn btn-primary btn-sm">Edit</button>
-                  <button type="button" class="btn btn-danger btn-sm">Hapus</button>
-                  <button type="button" class="btn btn-dark btn-sm">Detail</button>
-                
-                  </a>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div class="d-flex px-2 py-1">
-                    <div>
-
-                    </div>
-                    <div class="d-flex flex-column justify-content-center">
-                      <h6 class="mb-0 text-sm">4</h6>
-
-                    </div>
-                  </div>
-                </td>
-                <td>
-                  <p class="text-xs font-weight-bold mb-0">Citra</p>
-
-                </td>
-                <td class="align-middle text-center text-sm">
-                  <span class="badge badge-sm bg-gradient-success">0812345674</span>
-                </td>
-                <td class="align-middle text-center">
-                  <span class="text-secondary text-xs font-weight-bold">citra@gmail.com</span>
-                </td>
-                <td class="align-middle text-center">
-                  <span class="text-secondary text-xs font-weight-bold">admin1234</span>
-                </td>
-                <td class="align-middle">
-                  <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
-                  
-                  <button type="button" class="btn btn-primary btn-sm">Edit</button>
-                  <button type="button" class="btn btn-danger btn-sm">Hapus</button>
-                  <button type="button" class="btn btn-dark btn-sm">Detail</button>
-                
-                  </a>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div class="d-flex px-2 py-1">
-                    <div>
-
-                    </div>
-                    <div class="d-flex flex-column justify-content-center">
-                      <h6 class="mb-0 text-sm">5</h6>
-
-                    </div>
-                  </div>
-                </td>
-                <td>
-                  <p class="text-xs font-weight-bold mb-0">Fila</p>
-
-                </td>
-                <td class="align-middle text-center text-sm">
-                  <span class="badge badge-sm bg-gradient-success">0812345675</span>
-                </td>
-                <td class="align-middle text-center">
-                  <span class="text-secondary text-xs font-weight-bold">fila@gmail.com</span>
-                </td>
-                <td class="align-middle text-center">
-                  <span class="text-secondary text-xs font-weight-bold">admin1234</span>
-                </td>
-                <td class="align-middle">
-                  <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
-                  
-                  <button type="button" class="btn btn-primary btn-sm">Edit</button>
-                  <button type="button" class="btn btn-danger btn-sm">Hapus</button>
-                  <button type="button" class="btn btn-dark btn-sm">Detail</button>
-                
-                  </a>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div class="d-flex px-2 py-1">
-                    <div>
-
-                    </div>
-                    <div class="d-flex flex-column justify-content-center">
-                      <h6 class="mb-0 text-sm">6</h6>
-
-                    </div>
-                  </div>
-                </td>
-                <td>
-                  <p class="text-xs font-weight-bold mb-0">User Test</p>
-
-                </td>
-                <td class="align-middle text-center text-sm">
-                  <span class="badge badge-sm bg-gradient-success">0812345678</span>
-                </td>
-                <td class="align-middle text-center">
-                  <span class="text-secondary text-xs font-weight-bold">user@gmail.com</span>
-                </td>
-                <td class="align-middle text-center">
-                  <span class="text-secondary text-xs font-weight-bold">admin1234</span>
-                </td>
-                <td class="align-middle">
-                  <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
-                  
-                  <button type="button" class="btn btn-primary btn-sm">Edit</button>
-                  <button type="button" class="btn btn-danger btn-sm">Hapus</button>
-                  <button type="button" class="btn btn-dark btn-sm">Detail</button>
-                
-                  </a>
-                </td>
+              <?php if ($result->num_rows > 0): ?>
+                <?php while($row = $result->fetch_assoc()): ?>
+                  <tr>
+                    <td>
+                      <div class="d-flex px-2 py-1">
+                        <div class="d-flex flex-column justify-content-center">
+                          <h6 class="mb-0 text-sm"><?php echo $row['id_user']; ?></h6>
+                        </div>
+                      </div>
+                    </td>
+                    <td>
+                      <p class="text-xs font-weight-bold mb-0"><?php echo $row['nama']; ?></p>
+                    </td>
+                    <td class="align-middle text-center text-sm">
+                      <span class="badge badge-sm bg-gradient-success"><?php echo $row['no_telp']; ?></span>
+                    </td>
+                    <td class="align-middle text-center">
+                      <span class="text-secondary text-xs font-weight-bold"><?php echo $row['email']; ?></span>
+                    </td>
+                    <td class="align-middle text-center">
+                      <span class="text-secondary text-xs font-weight-bold">
+                        <?php echo str_repeat('•', strlen($row['password'])); ?></span>
+                      </td>
+                    <td class="align-middle text-center">
+                      <span class="badge badge-sm bg-gradient-info"><?php echo $row['role']; ?></span>
+                    </td>
+                    <td class="align-middle">
+                      <button type="button" class="btn btn-primary btn-sm">Edit</button>
+                      <button type="button" class="btn btn-danger btn-sm">Hapus</button>
+                    </td>
+                  </tr>
+                <?php endwhile; ?>
+              <?php else: ?>
+                <tr>
+                  <td colspan="7" class="text-center">Tidak ada data pengguna</td>
                 </tr>
-              
+              <?php endif; ?>
             </tbody>
           </table>
         </div>
       </div>
     </div>
-  </div>
+  </div>
 </div>
+<?php
+// Menutup koneksi database
+$koneksi->close();
+?>
