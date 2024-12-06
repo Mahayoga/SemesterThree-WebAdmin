dcc5cdc80b03eba44debadc13532763b87b55ff9
Mahayoga <myoga.bahtiar@gmail.com>
2024-12-06 11:03:53 +0700

    Fix, Change: struktur database

--- dependencies/index.php
+++ dependencies/index.php
@@ -4,19 +4,20 @@
     $databasePassword = '';
 
     if(!function_exists("readline")) {
-        echo "Masukkan localhost (database): ";
+        echo "Masukkan localhost (host database, misal, 'localhost'): ";
         $localhostName = trim(fgets(STDIN));
         echo "Masukkan username phpMyAdmin: ";
         $databaseUsername = trim(fgets(STDIN));
         echo "Masukkan password phpMyAdmin (kosongkan jika tidak ada): ";
         $databasePassword = trim(fgets(STDIN));
     } else {
-        $localhostName = readline('Masukkan localhost (database): ');
+        $localhostName = readline('Masukkan localhost (host database, misal, "localhost"): ');
         $databaseUsername = readline('Masukkan username phpMyAdmin: ');
         $databasePassword = readline('Masukkan password phpMyAdmin (kosongkan jika tidak ada): ');
     }
 
-    echo "Okee file config akan dibuat dengan nama database 'barbershop'.....\n";
+    $database = 'barbershop';
+    echo "Okee file config akan dibuat dengan nama database '$database'.....\n";
 
     mkdir('config');
     $fileConnection = fopen('config/connection.php', 'w');
@@ -28,12 +29,11 @@
 ?>');
     fclose($fileConnection);
 
-    $database = 'barbershop';
     $koneksi = new mysqli($localhostName, $databaseUsername, $databasePassword, '');
 
     // Dropping Database
     echo "Sedang me-reset database $database \n";
-    $sql = "DROP DATABASE $database";
+    $sql = "DROP DATABASE IF EXISTS $database";
     if($koneksi->query($sql) === true) {
         echo "Reset database berhasil!: $database \n";
     } else {
@@ -57,121 +57,29 @@
     }
 
     // Create Table Users
-$sql = "CREATE TABLE IF NOT EXISTS users (
-    id_user INT(11) AUTO_INCREMENT PRIMARY KEY,
-    nama VARCHAR(255) NOT NULL,
-    no_telp CHAR(12) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    role ENUM('admin', 'user') NOT NULL
-)";
-if ($koneksi->query($sql) === true) {
-    echo "Tabel dibuat!: users \n";
-} else {
-    echo "Error: saat membuat tabel \n";
-}
+    include "table_user.php";
 
-// Create Seeder users
-$sql = "INSERT INTO users (nama, no_telp, email, password, role) VALUES 
-    ('Mahayoga', '0812345671', 'myoga.bahtiar@gmail.com', '" . password_hash('admin1234', PASSWORD_BCRYPT, ['cost' => 12]) . "', 'admin'),
-    ('Irsyad','0812345672', 'syadd@gmail.com', '" . password_hash('admin1234', PASSWORD_BCRYPT, ['cost' => 12]) . "', 'admin'),
-    ('Nisa','0812345673', 'nisa@gmail.com', '" . password_hash('admin1234', PASSWORD_BCRYPT, ['cost' => 12]) . "', 'admin'),
-    ('Citra','0812345674', 'citra@gmail.com', '" . password_hash('admin1234', PASSWORD_BCRYPT, ['cost' => 12]) . "', 'admin'),
-    ('Fila','0812345675', 'fila@gmail.com', '" . password_hash('admin1234', PASSWORD_BCRYPT, ['cost' => 12]) . "', 'admin'),
-    ('User Test','0812345678', 'user@gmail.com', '" . password_hash('user1234', PASSWORD_BCRYPT, ['cost' => 12]) . "', 'user')
-    ";
-    if($koneksi->query($sql) === true) {
-        echo "Seeder users dibuat!: users \n";
-    } else {
-        echo "Error: saat memasukkan seeder users \n";
-    }
+    // Create Table Produk
+    include "table_produk.php";
 
-    // Create Table Users
-    $sql = "CREATE TABLE IF NOT EXISTS produk (
-        id_produk INT(11) AUTO_INCREMENT PRIMARY KEY,
-        nama_produk VARCHAR(255) NOT NULL,
-        harga_beli CHAR(12) NOT NULL,
-        harga_jual CHAR(12) NOT NULL,
-        deskripsi_produk VARCHAR(255) NOT NULL,
-        stok INT NOT NULL
-    )";
-    if($koneksi->query($sql) === true) {
-        echo "Tabel dibuat!: produk \n";
-    } else {
-        echo "Error: saat membuat tabel \n";
-    }
+    // Create Table Jasa
+    include "table_jasa.php";
 
-    // Create Seeder users
-    $sql = "INSERT INTO produk (nama_produk, harga_beli, harga_jual, deskripsi_produk, stok) VALUES 
-        ('Shampo','33000','40000','Shampo anti ketombe','45'),
-        ('Pomade','45000','55000','Pomade varians','55'),
-        ('Powder','47000','55000','Powder pelembut','30'),
-        ('Conditioner','30000','40000','Conditioner pelembut','40'),
-        ('Vitamin','70000','80000','Menutrisi dan merawat','47'),
-        ('Hairmask','55000','65000','Membantu melembutkan','30')
-    ";
-    if($koneksi->query($sql) === true) {
-        echo "Seeder produk dibuat! \n";
-    } else {
-        echo "Error: saat memasukkan seeder produk \n";
-    }
+    // Create Table Karyawan
+    include "table_karyawan.php";
 
-    // Create Table karyawan
-    $sql = "CREATE TABLE IF NOT EXISTS karyawan (
-        id_karyawan INT(11) AUTO_INCREMENT PRIMARY KEY,
-        nama_karyawan VARCHAR(255) NOT NULL,
-        no_telepon CHAR(15) NOT NULL,
-        alamat CHAR(12) NOT NULL
-       )";
-    if($koneksi->query($sql) === true) {
-        echo "Tabel dibuat!: karyawan \n";
-    } else {
-        echo "Error: saat membuat tabel \n";
-    }  
+    // Create Table Booking
+    include "table_booking.php";
 
-     // Create Seeder karyawan
-     $sql = "INSERT INTO karyawan (nama_karyawan, no_telepon, alamat) VALUES 
-     ('Mahayoga','085372638944','Probolinggo'),
-     ('Nisa','0813838452985','Banyuwangi'),
-     ('Citra','08324674634','Jember'),
-     ('Filla','0812445446','Probolinggo'),
-     ('Irsyad','08251575715','Gresik'),
-     ('Samsul','08327547252','Jember')
-     ";
-if($koneksi->query($sql) === true) {
-    echo "Seeder produk dibuat! \n";
-} else {
-    echo "Error: saat membuat tabel \n";
-}  
+    // Create Table Transaksi
+    include "table_transaksi.php";
 
-// Create Table Transaksi
-$sql = "CREATE TABLE IF NOT EXISTS transaksi (
-    transaksi_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    id_user INT(11) NOT NULL,
-    nama_customer VARCHAR(100) NOT NULL,
-    total_produk INT(3) NOT NULL,
-    total_harga DECIMAL(10,2) NOT NULL,
-    bayar DECIMAL(10,2) NOT NULL,
-    kembalian DECIMAL(10,2) NOT NULL,
-    payment_method ENUM('cash','transfer') DEFAULT 'cash',
-    tanggal_transaksi DATE NOT NULL,
-    status ENUM('completed','pending','failed') DEFAULT 'completed'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci";
-if ($koneksi->query($sql) === true) {
-    echo "Tabel dibuat!: transaksi \n";
-} else {
-    echo "Error: saat membuat tabel transaksi \n";
-}
+    // Create Table Detail Transaksi
+    include "table_detail_transaksi.php";
 
-// Create Seeder transaksi
-$sql = "INSERT INTO transaksi (id_user, nama_customer, total_produk, total_harga, bayar, kembalian, payment_method, tanggal_transaksi, status) VALUES
-    (3, 'Nisa', 2, 50000.00, 100000.00, 50000.00, 'cash', '2024-11-24', 'completed'),
-    (2, 'Irsyad', 4, 150000.00, 200000.00, 50000.00, 'cash', '2024-11-25', 'completed'),
-    (4, 'Citra', 3, 100000.00, 100000.00, 0.00, 'cash', '2024-11-25', 'completed')
-";
-if ($koneksi->query($sql) === true) {
-    echo "Seeder transaksi dibuat!: transaksi \n";
-} else {
-    echo "Error: saat memasukkan seeder transaksi \n";
-}
-?>
+    // Index
+    // Create Table Indexes
+    include "table_indexes.php";
+
+    // Create Foreign Key Table
+    include "table_foreign_key.php";
---
+++ dependencies/table_foreign_key.php
@@ -0,0 +1,21 @@
+<?php
+
+$sql = [
+    "ALTER TABLE transaksi ADD FOREIGN KEY (id_user) REFERENCES users (id_user)",
+    "ALTER TABLE transaksi ADD FOREIGN KEY (id_karyawan) REFERENCES karyawan (id_karyawan)",
+    "ALTER TABLE transaksi ADD FOREIGN KEY (id_karyawan) REFERENCES karyawan (id_karyawan)",
+    "ALTER TABLE detail_transaksi ADD FOREIGN KEY (id_produk) REFERENCES produk (id_produk)",
+    "ALTER TABLE detail_transaksi ADD FOREIGN KEY (id_jasa) REFERENCES jasa (id_jasa)",
+    "ALTER TABLE detail_transaksi ADD FOREIGN KEY (id_transaksi) REFERENCES transaksi (id_transaksi)",
+    "ALTER TABLE booking ADD FOREIGN KEY (id_user) REFERENCES users (id_user)",
+    "ALTER TABLE booking ADD FOREIGN KEY (id_jasa) REFERENCES jasa (id_jasa)",
+];
+
+foreach($sql as $item) {
+    if ($koneksi->query($item) === true) {
+    } else {
+        echo "Error: saat membuat tabel index foreign key!\n";
+    }
+}
+
+echo "Tabel index foreign key berhasil dibuat! \n";
