d9157b9a687546ad66db31cd010058e63dff7496
ayncitraa <ayunicitra001@gmail.com>
2024-12-09 12:43:32 +0700

    Embeded power BI to dashboard

--- pages/admin/dashboard.php
+++ pages/admin/dashboard.php
@@ -99,65 +99,26 @@
     </div>
   </div>
 </div>
-<div class="row mt-4">
-  <div class="col-lg-7 mb-lg-0 mb-4">
-    <div class="card z-index-2 h-100">
-      <div class="card-header pb-0 pt-3 bg-transparent">
-        <h6 class="text-capitalize">Grafik Bulanan</h6>
-        <p class="text-sm mb-0">
-          <i class="fa fa-arrow-up text-success"></i>
-          <span class="font-weight-bold">4% lebih</span> di 2024
-        </p>
-      </div>
-      <div class="card-body p-3">
-        <div class="chart">
-          <canvas id="chart-line" class="chart-canvas" height="300"></canvas>
+<div class="row mt-7">
+  <div class="col-lg-7">
+  <iframe title="Transaksi" width="1140" height="541.25" 
+              src="https://app.powerbi.com/reportEmbed?reportId=9c862a39-52b3-4c53-8b5b-fa5dcfeefc99&autoAuth=true&ctid=5263cc81-5912-42c4-abc1-d0f1b668b530" 
+              frameborder="0" allowFullScreen="true">
+            </iframe>
+          </div>
         </div>
       </div>
     </div>
   </div>
-  <div class="col-lg-5">
-    <div class="card card-carousel overflow-hidden h-100 p-0">
-      <div id="carouselExampleCaptions" class="carousel slide h-100" data-bs-ride="carousel">
-        <div class="carousel-inner border-radius-lg h-100">
-          <div class="carousel-item h-100 active" style="background-image: url('assets/img/carousel-1.jpg');
-      background-size: cover;">
-            <div class="carousel-caption d-none d-md-block bottom-0 text-start start-0 ms-5">
-              <div class="icon icon-shape icon-sm bg-white text-center border-radius-md mb-3">
-                <i class="ni ni-camera-compact text-dark opacity-10"></i>
-              </div>
-              <h5 class="text-white mb-1">Get started with Argon</h5>
-              <p>There’s nothing I really wanted to do in life that I wasn’t able to get good at.</p>
-            </div>
-          </div>
-          <div class="carousel-item h-100" style="background-image: url('assets/img/carousel-2.jpg');
-      background-size: cover;">
-            <div class="carousel-caption d-none d-md-block bottom-0 text-start start-0 ms-5">
-              <div class="icon icon-shape icon-sm bg-white text-center border-radius-md mb-3">
-                <i class="ni ni-bulb-61 text-dark opacity-10"></i>
-              </div>
-              <h5 class="text-white mb-1">Faster way to create web pages</h5>
-              <p>That’s my skill. I’m not really specifically talented at anything except for the ability to learn.</p>
-            </div>
-          </div>
-          <div class="carousel-item h-100" style="background-image: url('assets/img/carousel-3.jpg');
-      background-size: cover;">
-            <div class="carousel-caption d-none d-md-block bottom-0 text-start start-0 ms-5">
-              <div class="icon icon-shape icon-sm bg-white text-center border-radius-md mb-3">
-                <i class="ni ni-trophy text-dark opacity-10"></i>
-              </div>
-              <h5 class="text-white mb-1">Share with us your design tips!</h5>
-              <p>Don’t be afraid to be wrong because you can’t learn anything from a compliment.</p>
-            </div>
-          </div>
+    </div>
         </div>
-        <button class="carousel-control-prev w-5 me-3" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next w-5 me-3" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
+            <button class="carousel-control-prev w-5 me-3" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next w-5 me-3" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
         </button>
       </div>
     </div>
--- pages/layout/admin/sidebar.php
+++ pages/layout/admin/sidebar.php
@@ -58,7 +58,7 @@
         </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link <?php if(@$_GET['hal'] == 'billing') {echo 'active';}?>" href="?hal=billing">
+        <a class="nav-link <?php if(@$_GET['hal'] == 'kasir') {echo 'active';}?>" href="?hal=kasir">
           <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
             <i class="ni ni-credit-card text-dark text-sm opacity-10"></i>
           </div>
