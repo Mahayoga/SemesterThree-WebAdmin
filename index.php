1a6122c1b0c6aa3092db9cc451a8fb7eb5e96f14
Your 1rHub <ibadmuhammadirsyadul@gmail.com>
2024-12-13 20:09:47 +0700

    Add: User page

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
   if(@$_SESSION['isLogin']) {
     include 'pages/layout/admin/app.php';
   } else {
-    include 'pages/layout/user/app.php';
+    if(@$_GET['hal'] == 'login' || @$_GET['hal'] == 'register') {
+      include 'pages/layout/user/app.php';
+    } else {
+      include 'pages/layout/user/app2.php';
+    }
   }
 ?>
